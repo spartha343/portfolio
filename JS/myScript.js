d73ac780021e5b4d15faba32cd130a18e2a40613
spartha343 <spartha343@gmail.com>
2022-11-09 03:20:27 +0600

    form completed

--- JS/myScript.js
+++ JS/myScript.js
@@ -8,3 +8,39 @@ var tagcloud = TagCloud('.content', myTags, {
     direction: 135,
     keep: true
 });
+
+
+// form submission 
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.classList.add('success');
+            status.innerHTML = "Thanks for your submission!";
+            form.reset()
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+                    status.classList.add('error');
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form";
+                    status.classList.add('error');
+                }
+            })
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+}
+form.addEventListener("submit", handleSubmit)
